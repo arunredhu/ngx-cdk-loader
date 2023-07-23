140dc2da23a7b722c6ce6490a5944bf2fee29ee9
Arun Redhu <arun.redhu05@gmail.com>
2023-07-23 19:07:25 +0530

    Documentation added for Loader config

--- projects/ngx-cdk-loader/src/lib/models/ngx-cdk-loader-config.model.ts
+++ projects/ngx-cdk-loader/src/lib/models/ngx-cdk-loader-config.model.ts
@@ -1,3 +1,7 @@
+/**
+ * Loader configuration model
+ */
 export interface NgxCdkLoaderConfig {
+  /** Loader timeout in miliseconds. This is used to hide the loader after a specific timeout, if loader is still visible */
   loadingTimeout: number;
 }
