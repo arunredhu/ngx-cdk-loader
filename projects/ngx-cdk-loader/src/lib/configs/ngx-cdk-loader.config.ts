ac36792b65edd52a117f080858e9e64ca2518fc3
Arun Redhu <arun.redhu05@gmail.com>
2023-07-23 22:36:16 +0530

    Documentation added for loader config

--- projects/ngx-cdk-loader/src/lib/configs/ngx-cdk-loader.config.ts
+++ projects/ngx-cdk-loader/src/lib/configs/ngx-cdk-loader.config.ts
@@ -2,11 +2,13 @@ import { InjectionToken } from '@angular/core';
 
 import { NgxCdkLoaderConfig } from '../models';
 
+/** Default config for Ngx Loader */
 export const defaultNgxCdkLoaderConfig: NgxCdkLoaderConfig = {
   /** Default timeout for loader is 10 Seconds */
   loadingTimeout: 10000,
 };
 
+/** Injection token for config. Ngx Loader config will be provided on this injection token */
 export const NGX_CDK_LOADER_CONFIG = new InjectionToken(
   'NGX_CDK_LOADER_CONFIG'
 );
