11c938c85dfd4ba4164a82e354cbb256bed80c44
Arun Kumar <arunkumar@Nagarros-MacBook-Pro.local>
2023-07-23 12:14:00 +0530

    Public apis changed

--- projects/ngx-cdk-loader/src/public-api.ts
+++ projects/ngx-cdk-loader/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-cdk-loader
  */
 
-export * from './lib/ngx-cdk-loader.service';
-export * from './lib/ngx-cdk-loader.component';
 export * from './lib/ngx-cdk-loader.module';
+export * from './lib/services';
